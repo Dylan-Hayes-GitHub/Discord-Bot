e3894c38f6dc6ed7aba53a2f0ddc3b0e0d9ea4b5
Dylan-Hayes-GitHub <dylanmhayes12@gmail.com>
2023-05-13 13:41:06 +0100

    Updated embed, add try catch on fetch with retry of set timeout recursion

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 // require("dotenv").config();
 import dotenv from "dotenv";
 import { DiscordClient } from "./structures/Client";
+import { REST } from "discord.js";
 dotenv.config();
 
 export const client = new DiscordClient();
+export const rest = new REST({ version: "10" }).setToken(process.env.botId);
 client.start();
