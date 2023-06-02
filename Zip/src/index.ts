62700f9c3535efdbe6d51ce8676a48749dc0df2e
Dylan Hayes <73565932+Dylan-Hayes-GitHub@users.noreply.github.com>
2023-06-02 19:50:00 +0100

    Delete Zip directory

--- Zip/src/index.ts
+++
@@ -1,9 +0,0 @@
-// require("dotenv").config();
-import dotenv from "dotenv";
-import { DiscordClient } from "./structures/Client";
-import { REST } from "discord.js";
-dotenv.config();
-
-export const client = new DiscordClient();
-export const rest = new REST().setToken(process.env.botToken);
-client.start();
--- Zip/src/roles/Roles.ts
+++
@@ -1,8 +0,0 @@
-import Role from "./Role"
-
-export const roles: Role[] = [
-    new Role("Survival", "<@&1015563733470289931>"),
-    new Role("Interception","<@&1015564120906534975>"),
-    new Role("Disruption", "<@&1015564128649216020>"),
-    new Role("Excavation", "<@&1015564131039977493>")
-]
